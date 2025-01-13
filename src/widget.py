14f8a2de05b1ddb60bb6a1a96f46c9a56ea983be
Aleksander <sektorhack@gmail.com>
2025-01-13 21:48:14 +0300

    add def mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -5,8 +5,23 @@ from masks import get_mask_card_number
 
 
 
-def mask_account_card():
-    pass
+def mask_account_card(type_and_number: Union[str]) -> Union[str]:
+    """Функция, которая маскирует номер счета или карты"""
+
+    text_result = ""
+    digit_result = ""
+    digit_count = 0
+
+    for el in type_and_number:
+        if el.isalpha() or " ":
+            text_result += el
+        elif el.isdigit():
+            digit_result += el
+            digit_count += 1
+    if digit_count > 16:
+        return f"{text_result} {get_mask_account(digit_result)}"
+    else:
+        return f"{text_result} {get_mask_card_number(digit_result)}"
 
 
 def get_date(user_date: Union[str]) -> str:
