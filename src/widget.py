6c92d1d3145c80dccb68f400c555da6164d101fe
Aleksander <sektorhack@gmail.com>
2025-01-14 19:36:49 +0300

    edit widget.py

--- src/widget.py
+++ src/widget.py
@@ -5,26 +5,26 @@ from masks import get_mask_card_number
 
 
 
-def mask_account_card(type_and_number: Union[str]) -> Union[str]:
+def mask_account_card(type_and_number: Union[str]) -> str:
     """Функция, которая маскирует номер счета или карты"""
 
-    text_result = ""
-    digit_result = ""
-    digit_count = 0
-
-    for el in type_and_number:
-        if el.isalpha() or " ":
-            text_result += el
-        elif el.isdigit():
-            digit_result += el
-            digit_count += 1
-    if digit_count > 16:
-        return f"{text_result} {get_mask_account(digit_result)}"
+    split_input = type_and_number.split()
+    if len(split_input) == 2:
+        if split_input[0].lower() == "счет":
+            new_input = "".join(split_input[-1])
+            return f"{split_input[0]}  {get_mask_account(new_input)}"
+        else:
+            new_input = "".join(split_input[-1])
+            return f"{split_input[0]} {get_mask_card_number(new_input)}"
+    elif len(split_input) == 3:
+        new_input = "".join(split_input[-1])
+        return f"{split_input[0]} {split_input[1]} {get_mask_card_number(new_input)}"
     else:
-        return f"{text_result} {get_mask_card_number(digit_result)}"
+        print("Вы ввели неккоректные данные")
 
 
-print(mask_account_card(input()))
+print(mask_account_card(input("Введите имя и номер карты  ")))
+
 
 def get_date(user_date: Union[str]) -> str:
     """Функция получения даты в определенном формате и возвращения в формате ДД.ММ.ГГГГ
