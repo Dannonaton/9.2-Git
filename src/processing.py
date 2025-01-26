f2b74d545d2376be1d763b954ff8dd690f8e13b8
Aleksander <sektorhack@gmail.com>
2025-01-26 18:31:22 +0300

    add file processing.py

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+new_dictionary = []
+
+
+def filter_by_state(dictionary: list) -> list:
+    """Функция, для сортировки по ключу"""
+    for i in dictionary:
+        if i["state"] == "EXECUTED":
+            new_dictionary.append(i)
+    return new_dictionary
+
+
+def sort_by_date(sort_time: list, reverse: bool = True) -> list:
+    """Функция, для сортировки списка по дате"""
+    return sorted(sort_time, key=lambda x: x["date"], reverse=reverse)
