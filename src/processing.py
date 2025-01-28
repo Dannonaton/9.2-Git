11052b0daa99ef2c0033baaea08c2fc46bc95c14
Aleksander <sektorhack@gmail.com>
2025-01-28 18:37:42 +0300

    edit def filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
 new_dictionary = []
 
 
-def filter_by_state(dictionary: list) -> list:
+def filter_by_state(dictionary: list, state: str = 'EXECUTED') -> list:
     """ Функция, для сортировки по ключу """
     for i in dictionary:
-        if i["state"] == "EXECUTED":
+        if i["state"] == state:
             new_dictionary.append(i)
     return new_dictionary
 
@@ -12,3 +12,9 @@ def filter_by_state(dictionary: list) -> list:
 def sort_by_date(sort_time: list, reverse: bool = True) -> list:
     """ Функция, для сортировки списка по дате """
     return sorted(sort_time, key=lambda x: x["date"], reverse=reverse)
+
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
